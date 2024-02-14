128e32e3f571124175dc719c71d14b04ebd5857a
Øystein Langeland Sandvik <oysandvik94@gmail.com>
2024-02-14 19:59:47 +0100

    feat: store highest streak between games

---
+++ src/highscore_store.rs
@@ -0,0 +1,41 @@
+use std::{fs::{self, File}, io::{self, Write}, path::PathBuf};
+
+use dirs::data_local_dir;
+
+static GAME_DIR: &str = "symbol_typer";
+static HIGHSCORE_FILE: &str = "highscore";
+
+pub fn store_highscore(highscore: u32) -> io::Result<()> {
+    let file_path = get_highscore_filepath()?;
+
+    // Write or update the high score
+    let mut file = File::create(file_path)?;
+    file.write_all(highscore.to_string().as_bytes())?;
+
+    Ok(())
+}
+
+pub fn retrieve_highscore() -> io::Result<u32> {
+    let file_path = get_highscore_filepath()?;
+
+    // Write or update the high score
+    let highscore_file_content = fs::read_to_string(file_path)?;
+
+    let highscore = highscore_file_content
+        .parse::<u32>()
+        .unwrap_or(0);
+
+    Ok(highscore)
+}
+
+fn get_highscore_filepath() -> Result<PathBuf, io::Error> {
+    let mut data_path = data_local_dir().unwrap();
+    data_path.push(GAME_DIR);
+    if !data_path.exists() {
+        fs::create_dir_all(&data_path)?;
+    }
+    let mut file_path = PathBuf::from(&data_path);
+    file_path.push(HIGHSCORE_FILE);
+    Ok(file_path)
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
+mod highscore_store;
+
 use console::Term;
+use highscore_store::retrieve_highscore;
 use rand::seq::SliceRandom;
+
+use crate::highscore_store::store_highscore;
 
 fn main() {
     let symbols: Vec<char> = "!@#$%^&*()_+-=~\"{}'|;:,.<>?/".chars().collect();
 
+    let highest_streak: u32 = match retrieve_highscore() {
+        Ok(streak_result) => streak_result,
+        Err(error) => {
+            eprintln!("Failed to retrieve high score: {error}");
+            std::process::exit(1);
+        }
+    };
+
     loop {
         let symbol_to_match: char = *symbols.choose(&mut rand::thread_rng()).unwrap();
-        play_game(&symbols, symbol_to_match, 0, 0);
+        play_game(&symbols, symbol_to_match, 0, highest_streak);
     }
 }
 
@@ -18,13 +31,23 @@ fn play_game(symbols: &[char], symbol_to_match: char, streak: u32, highest_strea
     println!("Streak: {streak}. Highest streak: {highest_streak}");
     println!("{symbol_to_match}");
 
+    if let Err(error) = store_highscore(highest_streak) {
+        eprintln!("Failed to store high score: {error}");
+        std::process::exit(1);
+    }
+
     let term = Term::stdout();
     let character: char = term.read_char().expect("Should be a character");
 
     match test_characters(character, symbol_to_match) {
         RoundResult::Correct => {
             println!("Correct!");
-            play_game(symbols, pick_symbol(symbols), streak + 1, std::cmp::max(streak, highest_streak))
+            play_game(
+                symbols,
+                pick_symbol(symbols),
+                streak + 1,
+                std::cmp::max(streak, highest_streak),
+            )
         }
         RoundResult::Incorrect { played, target } => {
             println!("Feil!! Du skrev {} men det var {}", played, target);
